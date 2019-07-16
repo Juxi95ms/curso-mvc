8a8c64977d1ce8efbcc9441036b2eb82f1469724
Juxi <julianms1995@gmail.com>
2019-07-16 12:47:03 -0500

    Se agrego el controlador Home

---
+++ MiPrimeraAplicacion/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimeraAplicacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index()
+        {
+            return "Mi primera aplicacion en ASP.NET con MVC5";
+        }
+    }
+}
